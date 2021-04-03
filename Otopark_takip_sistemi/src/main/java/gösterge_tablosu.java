f84f9068949a27955d7e34680931be0e998bbfee
Emir Güler <77588741+guleremir@users.noreply.github.com>
2021-04-03 18:30:42 +0300

    Otomasyon Projesi
    Otopark otomasyonu

---
+++ Otopark_takip_sistemi/src/main/java/gösterge_tablosu.java
@@ -0,0 +1,20 @@
+
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MONSTER
+ */
+class gösterge_tablosu {
+
+    static void setModel(DefaultTableModel tablo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
